c5775bc2b4dfd6c03fa84b4981f5baddb35be53e
Daniela Pierce <mdgarciaaguilar@hotmail.com>
2019-03-19 18:58:15 -0600

    Initial Commit

---
+++ ProyFinal/ViewControllerDarCarta.swift
@@ -0,0 +1,33 @@
+//
+//  ViewControllerDarCarta.swift
+//  ProyFinal
+//
+//  Created by Carlos Pérez 2 on 3/14/19.
+//  Copyright © 2019 Carlos Pérez 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerDarCarta: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
