b08eacf1c2c8887b68df7ab2038e0e9afa11eafb
Daniela Pierce <mdgarciaaguilar@hotmail.com>
2019-03-20 12:48:01 -0600

    General: Meti algunas imágenes de iconos al proyecto, no son las 54, pero son algunas Modo dar la carta: Ya se puede dar la carta, al picarle al botón de siguiente carta aparece la siguiente carta. Al picarle al botón de lista de cartas, te lleva a un tableViewController con la lista de cartas que ya se dieron.

--- ProyFinal/ViewControllerDarCarta.swift
+++ ProyFinal/ViewControllerDarCarta.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewControllerDarCarta: UIViewController {
+    
+    @IBOutlet weak var imgCarta: UIImageView!
+    
+    var listaCartas = [String](arrayLiteral: "taza", "baston", "pastillero", "jarabe", "cafe", "termometro", "cama", "libros", "juegos de mesa", "enfermero", "inyeccion", "pastillas", "suero", "reloj", "lentes", "bolsa", "dentadura", "sillon", "lupa", "pijamas", "cepillo", "maletin", "pantunflas", "bufanda", "guantes", "camion", "fotos", "camara", "sombrero")
+    
+    var listaCartasDadas = [String]()
+    
+    
+    var index : Int = 0
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +28,36 @@ class ViewControllerDarCarta: UIViewController {
         // Do any additional setup after loading the view.
         
         self.title = "Dar la carta"
+        
+        listaCartas.shuffle()
+        imgCarta.image = UIImage(named: listaCartas[index])
+        listaCartasDadas.append(listaCartas[index])
+        listaCartas.remove(at: index)
+        index = index + 1
+        
+        
     }
     
 
     
+    @IBAction func siguienteCarta(_ sender: UIButton) {
+        imgCarta.image = UIImage(named: listaCartas[index])
+        listaCartasDadas.append(listaCartas[index])
+        listaCartas.remove(at: index)
+        index = index + 1
+    }
     
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let vistaCartasDadas = segue.destination as! TableViewControllerCartasDadas
+        vistaCartasDadas.listaCartasYaDadas = listaCartasDadas
     }
-    */
+    
 
 }
