3fb47982340928e7e203dc5a11d3ee33d4a41098
Alumno <alumno@laboratorios-macbook-pro-24.local>
2019-05-02 13:06:52 -0500

    como jugar - modo carta sin contraints

---
+++ ProyFinal/ViewControllerComoJugar.swift
@@ -0,0 +1,31 @@
+//
+//  ViewControllerComoJugar.swift
+//  ProyFinal
+//
+//  Created by Alumno on 5/2/19.
+//  Copyright © 2019 Carlos Pérez 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerComoJugar: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Cómo Jugar"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
